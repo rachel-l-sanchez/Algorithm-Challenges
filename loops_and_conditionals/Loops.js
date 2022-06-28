9f4ac9e632e64b38011cec827918f38896b55c2c
Rachel Sanchez <rachel.sanchez@live.com>
2022-06-27 18:10:55 -0700

    Loops and conditionals

---
+++ loops_and_conditionals/Loops.js
@@ -0,0 +1,52 @@
+//odds 1-20
+function odds() {
+    for (var i =1; i<21; i++) {
+        if (i % 2 ==1) {
+            console.log(i);
+        }
+    }
+}
+
+odds();
+
+//decreasing multiples
+function multiples () {
+    for (var j=100; j<=0; j--) {
+        if (i % 3 == 0) {
+            console.log(i);
+        }
+    }
+}
+
+multiples();
+
+//print the sequence of -1.5
+function subtract () {
+    for (var k=4; k<=-3.5; k-=1.5) {
+        console.log(i);
+    }
+}
+
+subtract();
+
+//sigma function
+let sum=0;
+function sigma () {
+    for (var l=0; l<101; l++) {
+        sum+= l;
+    }
+}
+sigma();
+
+let product = 1;
+
+function factorial() {
+    for (var m=1; m<=12; m++) {
+        product = product*m;
+    }
+}
+
+factorial();
+
+
+
