2806a50b4f9fc49a3f264de19687fee56478c1b2
Rachel Sanchez <rachel.sanchez@live.com>
2022-07-18 19:47:24 -0700

    init

--- FizzBuzz/Fizzbuzz.js
+++ FizzBuzz/Fizzbuzz.js
@@ -3,7 +3,7 @@ function fizzBuzz () {
         if (i % 15 === 0) {
             console.log("FizzBuzz");
         }
-        else if (i % 5 === 0) {
+        if (i % 5 === 0) {
             console.log("Buzz");
         }
         else if (i % 3 === 0) {
